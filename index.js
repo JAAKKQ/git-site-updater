66106d05a7a490b12557e1482b5e31bb6c7a311d
Jaakko <mail@r3ne.net>
2022-12-20 15:20:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const server = http.createServer((request, response) => {
             const payload = JSON.parse(body);
             console.log(payload);
             // Check if the event type is a push event
-            if (JSON.stringify(payload.forks_url).replace("/forks", "") === config.repoUrl) {
+            if (true) {
                 console.log("Push event");
                 // Execute the JavaScript code you want to run in response to the push event
                 https.get(config.repoUrl, options, (response) => {
